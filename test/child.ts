d86a5469bd9bcbe8c024248148d0d0a3f2d43c05
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-12-15 03:25:03 +0800

    testing parallelism api

---
+++ test/child.ts
@@ -0,0 +1,20 @@
+process.on('message', message => {
+  console.log('from parent:', message);
+  if (message === 'stop') {
+    // console.log('child closing')
+    // process.send('closed')
+    // process.exit()
+    setTimeout(() => {
+      console.log('child is still active 2');
+      setTimeout(() => {
+        console.log('child is still active 1');
+        setTimeout(() => {
+          console.log('child is closing now');
+          process.exit(0);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+    return;
+  }
+  process.send({ result: 2 });
+});
---
+++ test/parallel-test.ts
@@ -0,0 +1,10 @@
+import { fork } from 'child_process';
+
+let child = fork('./child.js');
+child.send({ data: 1 });
+child.on('message', message => {
+  console.log('from child:', message);
+  child.send('stop');
+  console.log('parent closing');
+  process.exit();
+});
